8cf2c0bdf1dc9dfc36c6c9bbc3dc91af1668125a
strafe <strafe@users.noreply.github.com>
2021-03-22 21:30:52 +0000

    fix: adjust sed pattern to work when on battery power too

--- lib/sh/bat.sh
+++ lib/sh/bat.sh
@@ -1,1 +1,1 @@
-printf "$(pmset -g batt | awk {'print $3 $4'} | sed -e s/from\'// -e s/%\;charged\;//)"
+printf "$(pmset -g batt | awk {'print $3 $4'} | sed -Ee s/from\'// -e s/%\;\(charged\|discharging\)\;//)"
