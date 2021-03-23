1c31d6b7edd840cf86438a9a5dbefc3612188897
strafe <strafe@users.noreply.github.com>
2021-03-23 02:05:58 +0000

    fix: account for finishing charge state in bat.sh

--- lib/sh/bat.sh
+++ lib/sh/bat.sh
@@ -1,1 +1,1 @@
-printf "$(pmset -g batt | awk {'print $3 $4'} | sed -Ee s/from\'// -e s/%\;\(charged\|charging\|discharging\)\;//)"
+printf "$(pmset -g batt | awk {'print $3 $4'} | sed -Ee s/from\'// -e s/%\;\(charged\|charging\|discharging\|finishing[[:space:]]charge\)\;//)"
